fee74bac0b719961eb716e539e54e861c399b3bf
arjuna26 <97765080+arjuna26@users.noreply.github.com>
2025-07-31 18:39:11 -0700

    session persist

--- mobile-app/utils/supabase.js
+++ mobile-app/utils/supabase.js
@@ -1,6 +1,13 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        storage: AsyncStorage,
+    },
+});
